46de345a33b565677b020931aff0208e31119518
gabriel <gabrielcasafu@gmail.com>
2022-03-09 09:43:43 -0300

    Reutilizacion de repo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component} from 'react';
+import {Input } from 'reactstrap';
 
-import Weather from "./Component/Weather";
-import Search from "./Component/Search";
-import Footer from "./Component/Footer";
-import Banner from "./Component/Banner";
 import 'weathericons/css/weather-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Api_Key = "39bdd30d5ff4dcb293669437108f69cf";
+const api = {
+  key: "6f37e51f54f144d7b38181046220803",
+  base: "http://api.weatherapi.com/v1/forecast.json",
+}
 
 class App extends Component{
   
@@ -15,157 +15,176 @@ class App extends Component{
     super();
     
     this.state={
-      city: undefined,
-      country: undefined,
-      temp: '',
-      temp_max: '',
-      temp_min: '',
-      pressure: '',
-      humidity: '',
-      visibility: '',
-      descripcion: '',
-      dt:'',
-      //currentCity: '',
-      error: false
-    };
-
-    this.weatherIcon = {
-      Thunderstorm: "wi-thunderstorm",
-      Drizzle: "wi-sleet",
-      Rain: "wi-storm-showers",
-      Snow: "wi-snow",
-      Atmosphere: "wi-fog",
-      Clear: "wi-day-sunny",
-      Clouds: "wi-day-fog"
+      city: '',
+      location: '',
+      weatherAndForecastsDays: [],
+      locations: [{id: 1,name: "Bariloche"}, {id: 2,name: "Mendoza"}, {id: 3,name: "Ushuaia"}, {id: 4,name: "Posadas"}, {id: 5,name: "San Juan"}]
     };
 
   }
 
-  get_WeatherIcon(icons, rangeId) {
-    switch (true) {
-      case rangeId >= 200 && rangeId < 232:
-        this.setState({ icon: icons.Thunderstorm });
-        break;
-      case rangeId >= 300 && rangeId <= 321:
-        this.setState({ icon: icons.Drizzle });
-        break;
-      case rangeId >= 500 && rangeId <= 521:
-        this.setState({ icon: icons.Rain });
-        break;
-      case rangeId >= 600 && rangeId <= 622:
-        this.setState({ icon: icons.Snow });
-        break;
-      case rangeId >= 701 && rangeId <= 781:
-        this.setState({ icon: icons.Atmosphere });
-        break;
-      case rangeId === 800:
-        this.setState({ icon: icons.Clear });
-        break;
-      case rangeId >= 801 && rangeId <= 804:
-        this.setState({ icon: icons.Clouds });
-        break;
-      default:
-        this.setState({ icon: icons.Clouds });
-    }
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      this.setState({ location })
+      this.geoReferencia();
+    }, (err) => {
+      console.log(err.message)
+    })
   }
 
-  //convierto a grados celsius
-  calCelsius(temp){
-    let cell = Math.floor(temp-273,15);
-    return cell;
+  changeCity = e => {
+
+    this.setState({
+      city: e.target.value,
+    }, () => {
+      this.consultarDatos();
+    })
   }
 
-  //formateo fecha
+  consultarDatos() {
+    fetch(`${api.base}?key=${api.key}&q=${this.state.city }&days=5&aqi=no&alerts=no&lang=es`)
+        .then(res => res.json())
+        .then(result => {
+          this.setState({
+            weatherAndForecastsDays: result,
+          }) 
+        });
+  }
+
+  geoReferencia() {
+    const { latitude } = this.state.location.coords;
+    const { longitude } = this.state.location.coords;
+    fetch(`${api.base}?key=${api.key}&q=${latitude},${longitude}&days=5&aqi=no&alerts=no&lang=es`)
+        .then(res => res.json())
+        .then(result => {
+          this.setState({
+            weatherAndForecastsDays: result,
+          }) 
+        });
+  }
+           
+  dateBuilder = (d) => {
+    let months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+    let days = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "sabado"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} de ${month} de ${year}`
+  }
+
   format_time(s) {
-    let day = new Date(s * 1e3).toLocaleDateString();
-    let hora = new Date(s * 1e3).toLocaleTimeString();
-    return  "Pronóstico oficial del día " + day + " última actualizacion a las " + hora + " horas.";
+    let day = new Date(s).toLocaleDateString();
+    return  day.slice(0, -5) ;
   }
-
-  
-  getWeather = async e => {
-    
-    e.preventDefault();
-
-    const country = e.target.elements.country.value;
-    const city = e.target.elements.city.value;
-
-    if (country && city) {
-      const api_call = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}&lang=sp`
-        //`http://api.openweathermap.org/data/2.5/weather?q=London,Uk&appid=${Api_Key}`
-      );
-
-      const response = await api_call.json();
-
-      this.setState({
-        city: response.name,
-        country: response.sys.country,
-        main: response.weather[0].main,
-        temp: this.calCelsius(response.main.temp),
-        temp_max: this.calCelsius(response.main.temp_max),
-        temp_min: this.calCelsius(response.main.temp_min),
-        pressure: response.main.pressure,
-        humidity: response.main.humidity,
-        visibility: response.visibility,
-        descripcion: response.weather[0].description,
-        dt: this.format_time(response.dt),
-        error: false
-
-      });
-
-      // seting icons
-      this.get_WeatherIcon(this.weatherIcon, response.weather[0].id);
-
-      console.log(response);
-    } else {
-      this.setState({
-        error: true
-      });
-    }
-  };
 
   render(){
 
+  const { 
+      city,
+      weatherAndForecastsDays,
+      locations,
+      location
+  } = this.state;
+
     return(
-      <div className="hold-transition skin-blue layout-top-nav">
-        <div className="wrapper">
-
-          <Banner 
-           city={this.state.city} 
-           country={this.state.country}
-           temp={this.state.temp} 
-           weatherIcon={this.state.icon}
-          />  
-          <div className="content-wrapper">
-              <div className="container"> 
-                  <section className="content text-center">
-                      <Search 
-                        loadweather={this.getWeather}
-                        error={this.state.error}
-                      />
-                      <Weather 
-                        city={this.state.city} 
-                        country={this.state.country} 
-                        temp={this.state.temp} 
-                        temp_max={this.state.temp_max} 
-                        temp_min={this.state.temp_min} 
-                        pressure={this.state.pressure} 
-                        humidity={this.state.humidity} 
-                        visibility={this.state.visibility}
-                        descripcion={this.state.descripcion}
-
-                        weatherIcon={this.state.icon}
-                      />
-                      <Footer 
-                        dt={this.state.dt}
-                      />
-
-                  </section>  
+      <div>
+      <main>
+        <div className="search-box">
+          <Input
+              type="select"
+              className="form-control"
+              onChange={this.changeCity}
+              value={city}
+          >
+              <option value=''>Seleccionar ciudad</option>
+              {
+                  locations && locations.map(location =>( <option key={location.id} value={location.name}>{location.name}</option> ) )
+              }
+          </Input>
+        </div>
+        
+        {
+          (weatherAndForecastsDays != '') ? (
+          <div>
+            <div className="content-box">
+              <div className="left-box">
+                  <div className='title'>
+                    EL TIEMPO AHORA EN <span className='title-localization'>{weatherAndForecastsDays.location && weatherAndForecastsDays.location.name}</span><br></br>
+                    <span className='date'>{this.dateBuilder(new Date())}</span>
+                  </div>
+                  
+                  <div className='weather-current'>
+                      <div className='weather-image'>
+                          <img src={weatherAndForecastsDays.current && weatherAndForecastsDays.current.condition.icon} alt="weather" width="64" height="64"/>
+                      </div>
+                      <div className='weather-temp'>
+                        {weatherAndForecastsDays.current && weatherAndForecastsDays.current.temp_c}°<span className='weather-temp-icon'>C</span>
+                      </div>
+                  </div>
+                  <div className='title-data'>{weatherAndForecastsDays.current && weatherAndForecastsDays.current.condition.text}</div>
               </div>
-          </div> 
+              <div className="right-box">
+                <div className='weather-current'>
+                    <div className='weather-data'>
+                        <p className='line-separate'>Humedad: <span className='data-right'>{weatherAndForecastsDays.current && weatherAndForecastsDays.current.humidity}%</span></p>
+                        <p className='line-separate'>Presion: <span className='data-right'>{weatherAndForecastsDays.current && weatherAndForecastsDays.current.pressure_mb} hPa</span></p>
+                        <p className='line-separate'>Velocidad del Viento: <span className='data-right'>{weatherAndForecastsDays.current && weatherAndForecastsDays.current.wind_dir} - {weatherAndForecastsDays.current && weatherAndForecastsDays.current.wind_kph}km/h</span></p>
+                        <p>Visibilidad: <span className='data-right'>{weatherAndForecastsDays.current && weatherAndForecastsDays.current.vis_km}km</span></p>
+                    </div>
 
-      </div>
+                </div>
+              </div>
+
+              <div className="content-box-forecast">
+
+                <div className='title-forecast'>PREVISION PROXIMOS 3 DÍAS</div>
+
+                {
+                  
+                      weatherAndForecastsDays.forecast && weatherAndForecastsDays.forecast.forecastday.map((comprobante, index) => {
+                        return (
+                          <div className="full-box">
+                              <div className='date-forecast'>
+                                {comprobante && this.format_time(comprobante.date)}
+                              </div> 
+
+                              <div className='date-forecast'>
+                                    <img src={comprobante && comprobante.day.condition.icon}  alt="Forecasts" width="64" height="64"/>
+                              </div>
+
+                              <div className='date-forecast'>
+                                <span className='date-forecast-title'>{comprobante && comprobante.day.maxtemp_c}°</span> / {comprobante && comprobante.day.mintemp_c}°
+                              </div>
+
+                              <div className='date-forecast'>
+                                {comprobante && comprobante.day.condition.text}
+                              </div>
+                          </div>
+
+                    );
+                  })
+                  
+                  
+                }
+                
+
+            </div>
+
+
+            </div>
+
+          </div>
+                      
+          
+
+        ) : ('')
+        
+        
+        }
+      </main>
     </div>
     );
   }
